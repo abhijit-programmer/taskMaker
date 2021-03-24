dfa204338bb55f054ccc0bf65960b81a7b0fc828
abhijit-programmer <coder.abhijit90@gmail.com>
2021-03-24 08:22:09 +0530

    Fix ui of list

--- src/components/List.js
+++ src/components/List.js
@@ -7,14 +7,14 @@ const List = () => {
 	const [error, setError] = useState(false);
 
 	// Date formate function
-  const formateDate = (date) => {
-    let newDate = date.slice(0, 10);
-    newDate = newDate.split("-");
-    newDate = newDate.reverse();
-    newDate = newDate.join("-");
+	const formateDate = (date) => {
+		let newDate = date.slice(0, 10);
+		newDate = newDate.split("-");
+		newDate = newDate.reverse();
+		newDate = newDate.join("-");
 
-    return newDate;
-  };
+		return newDate;
+	};
 
 	const preload = () => {
 		getAllTasks().then(data => {
@@ -36,29 +36,31 @@ const List = () => {
 				{tasks &&
 					tasks.map((task, index) => (
 						<li className="list-group-item list-group-item-action">
-							{task.name}
+							<span className="d-block d-md-inline d-lg-inline">{task.name}</span>
 							{/* <span className="btn btn-sm btn-info float-right ml-2">Update</span> */}
-							<span
-								type="button"
-								className="dropdown-toggle dropdown-toggle-split float-right ml-1"
-								data-toggle="dropdown"
-								aria-haspopup="true"
-								aria-expanded="false"
-							>
-							</span>
-							<div className="dropdown-menu">
-								<Link className="dropdown-item" to={`/update/${task._id}`}>
-									Update Status
+							<span className="d-block d-md-inline d-lg-inline float-lg-right float-md-right">
+								<span className="badge badge-danger">
+									{task.priority}
+								</span>
+								<span className="badge badge-warning ml-1">
+									{formateDate(task.due_date)}
+								</span>
+								<span className="badge badge-success ml-1">
+									{task.status}
+								</span>
+								<span
+									type="button"
+									className="dropdown-toggle dropdown-toggle-split ml-1"
+									data-toggle="dropdown"
+									aria-haspopup="true"
+									aria-expanded="false"
+								>
+								</span>
+								<div className="dropdown-menu">
+									<Link className="dropdown-item" to={`/update/${task._id}`}>
+										Update Status
 								</Link>
-							</div>
-							<span className="badge badge-success ml-1 float-right">
-								{task.status}
-							</span>
-							<span className="badge badge-warning ml-1 float-right">
-								{formateDate(task.due_date)}
-							</span>
-							<span className="badge badge-danger ml-1 float-right">
-								{task.priority}
+								</div>
 							</span>
 						</li>
 					))}
